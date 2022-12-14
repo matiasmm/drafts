4de8553c4df2930f20009ff7ad49efc3dd5f3ec4
Matias Montenegro <matias@rzerosystems.com>
2022-12-14 13:48:37 -0300

    playing with naudiodon

---
+++ portaudio/README.md
@@ -0,0 +1,4 @@
+## Troubleshooting installation
+
+I had problems with installation, but this sorted it out.
+https://gist.github.com/adrienjoly/e29a6e73fb7e701eefd80ff9bde9abeb#file-fix-dyld-missing-symbol-called-errors-on-m1-macs-md
---
+++ portaudio/portaudio.js
@@ -0,0 +1,47 @@
+var portAudio = require('naudiodon');
+var fs = require('fs');
+
+const DEV_AIRPODS = 'AirPods Max de Matias'
+const DEV_VB_CABLE = 'VB-Cable'
+const DEV_SPEAKERS = 'MacBook Pro Speakers'
+const DEV_MIC_MAC = 'MacBook Pro Microphone'
+
+const INPUT_DEVICE_NAME = DEV_VB_CABLE
+const OUTPUT_DEVICE_NAME = DEV_AIRPODS
+
+const inputDevice = portAudio.getDevices().find(device => device.name === INPUT_DEVICE_NAME && device.maxInputChannels > 0)
+const outputDevice = portAudio.getDevices().find(device => device.name === OUTPUT_DEVICE_NAME && device.maxOutputChannels > 0)
+
+var ai = new portAudio.AudioIO({
+    inOptions: {
+      channelCount: 2,
+      sampleFormat: portAudio.SampleFormat16Bit,
+      sampleRate: 44100,
+      deviceId: inputDevice.id, // Use -1 or omit the deviceId to select the default device
+      closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
+
+    },
+    outOptions: {
+      channelCount: 2,
+      sampleFormat: portAudio.SampleFormat16Bit,
+      sampleRate: 44100,
+      deviceId: outputDevice.id, // Use -1 or omit the deviceId to select the default device
+      closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
+    }
+  });
+
+
+
+// var ws = fs.createWriteStream('rawAudio2.raw');
+
+// ai.pipe(ws);
+ai.start();
+
+ai.on('close', buf => console.log('close',buf));
+ai.on('data', buf => console.log('data',buf));
+ai.on('error', buf => console.log('error', buf));
+
+// process.on('SIGINT', () => {
+//     console.log('Received SIGINT. Stopping recording.');
+//     ai.quit();
+//   });
