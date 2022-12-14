a3d2ec797465ac1a29e6a91b4a478c8ce57c4a61
Matias Montenegro <matias@rzerosystems.com>
2022-12-14 14:22:16 -0300

    audio router working fine

--- portaudio/portaudio-manual-io.js
+++ portaudio/portaudio-manual-io.js
@@ -1,49 +1,64 @@
+// Inspired in https://github.com/Streampunk/naudiodon/issues/13#issuecomment-384928879
+
 var portAudio = require('naudiodon');
 var fs = require('fs');
 
 const DEV_AIRPODS = 'AirPods Max de Matias'
+const DEV_BLACKHOLE = 'BlackHole 2ch'
 const DEV_VB_CABLE = 'VB-Cable'
 const DEV_SPEAKERS = 'MacBook Pro Speakers'
 const DEV_MIC_MAC = 'MacBook Pro Microphone'
 
-const INPUT_DEVICE_NAME = DEV_VB_CABLE
+const INPUT_DEVICE_NAME = DEV_BLACKHOLE
 const OUTPUT_DEVICE_NAME = DEV_AIRPODS
 
 const inputDevice = portAudio.getDevices().find(device => device.name === INPUT_DEVICE_NAME && device.maxInputChannels > 0)
 const outputDevice = portAudio.getDevices().find(device => device.name === OUTPUT_DEVICE_NAME && device.maxOutputChannels > 0)
 
-var ai = new portAudio.AudioIO({
-    inOptions: {
-      channelCount: 2,
-      sampleFormat: portAudio.SampleFormat16Bit,
-      sampleRate: 44100,
-      deviceId: inputDevice.id, // Use -1 or omit the deviceId to select the default device
-      closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
+var ao = new portAudio.AudioIO({    
+  outOptions:   {
+    channelCount: 2,
+    sampleFormat: portAudio.SampleFormat16Bit,
+    sampleRate: 16000,
+    deviceId: outputDevice.id
+  }
+});
 
-    },
-    outOptions: {
-      channelCount: 2,
-      sampleFormat: portAudio.SampleFormat16Bit,
-      sampleRate: 44100,
-      deviceId: outputDevice.id, // Use -1 or omit the deviceId to select the default device
-      closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
-    }
-  });
+var ai = new portAudio.AudioIO({    
+  inOptions:   {
+    channelCount: 2,
+    sampleFormat: portAudio.SampleFormat16Bit,
+    sampleRate: 16000,
+    deviceId: inputDevice.id
+  }
+});
 
 
 
-// var ws = fs.createWriteStream('rawAudio2.raw');
+// ai.pipe(ao); //  produces audio overflow
 
-console.log('xx', ai)
+/** INPUT events */
+ai.on("data", chunk => {
+  //console.log(chunk)
+   ao.write(chunk) 
+});
 
-// ai.pipe(ws);
 ai.start();
 
-ai.on('close', buf => console.log('close',buf));
-ai.on('data', buf => console.log('data',buf));
-ai.on('error', buf => console.log('error', buf));
 
-// process.on('SIGINT', () => {
-//     console.log('Received SIGINT. Stopping recording.');
-//     ai.quit();
-//   });
+/** Output events */
+ao.on("error", chunk => {
+  console.error("ERROR OUTPUT", chunk) 
+});
+ao.start();
+
+
+
+
+process.on('SIGINT', () => {
+    console.log('Received SIGINT. Stopping recording.');
+    ai.quit();
+    console.log('Stopped AI');
+    ao.quit()
+    console.log('Stopped AO');
+  });
