51d3d9c6d210b8b27404f67a9d97ef85115276ad
Matias Montenegro <matias@rzerosystems.com>
2023-03-17 16:56:25 -0300

    microframeworks

---
+++ micro-frameworks/helloReact/src/App.js
@@ -0,0 +1,17 @@
+// helloReact/src/App.js
+import React from 'react'
+import { Switch, Route, Router } from 'react-router-dom'
+
+const helloWorld = () => (<div>Hello World!</div>)
+const helloReact = () => (<div>Hello React!</div>)
+
+export default ({ history }) => {
+    return <div>
+        <Router history={history}>
+            <Switch>
+                <Route path="/react" component={helloReact} />
+                <Route path="/" component={helloWorld} />
+            </Switch>
+        </Router>
+    </div>
+}
